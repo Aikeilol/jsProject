68c3e20518ade85ce49227c6371a3f18cfda8fca
Aikeilol <garena3307@mail.ru>
2021-12-24 00:03:11 +0300

    post request

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
-
-    const tabs = document.querySelectorAll(".tabheader__item"),//menu
-        tabsContent = document.querySelectorAll(".tabcontent"),//img and description
-        tabsParent = document.querySelector(".tabheader__items");//parent
+    //TABS
+    const tabs = document.querySelectorAll(".tabheader__item"), //menu
+        tabsContent = document.querySelectorAll(".tabcontent"), //img and description
+        tabsParent = document.querySelector(".tabheader__items"); //parent
 
     function hideTabContent() {
         tabsContent.forEach(item => {
             item.classList.add('hide');
-            item.classList.remove("show","fade");
+            item.classList.remove("show", "fade");
         });
         tabs.forEach(item => {
             item.classList.remove("tabheader__item_active");
@@ -34,4 +34,142 @@ window.addEventListener("DOMContentLoaded", () => {
             });
         }
     });
+
+    //TIMER
+
+    const deadline = '2022-03-12';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+
+    }
+
+    function zero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+            days.innerHTML = zero(t.days);
+            hours.innerHTML = zero(t.hours);
+            minutes.innerHTML = zero(t.minutes);
+            seconds.innerHTML = zero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+    //MODAL
+
+    const modalTrigger = document.querySelectorAll("[data-modal]"),
+        modal = document.querySelector('.modal');
+
+    modalTrigger.forEach(item => {
+        item.addEventListener('click', (e) => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function hideModal(modalWindow) {
+        modalWindow.classList.add('hide');
+        modalWindow.classList.remove('show');
+        document.body.style.overflow = 'visible';
+    }
+
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.getAttribute('data-close') == '' ) {
+            hideModal(modal);
+        }
+    });
+
+    document.addEventListener('keydown', (e) =>{
+        if(e.code === 'Escape' && modal.classList.contains('show')){
+            hideModal(modal);
+        }
+    });
+    
+    //Forms post
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item =>{
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMassage = document.createElement('div');
+            statusMassage.classList.add('status');
+            statusMassage.textContent = message.loading;
+            form.append(statusMassage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST','server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form); //если установлен FormData то нам не нужно setRequestHeader('Content-type', 'multipart/form-data') если отправляем джейсон то нужно request.setRequestHeader('Content-type', 'application/json');
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if(request.status === 200){
+                    console.log(request.response);
+                    statusMassage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() =>{
+                        statusMassage.remove();
+                    },2000);
+                }else{
+                    statusMassage.textContent = message.failure;
+                }
+            });
+
+        
+        });
+    }
 });
