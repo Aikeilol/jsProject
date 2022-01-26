aaddd6ac4cc203d8d76fdd6dab53fb1f89b8e78a
Aikeilol <garena3307@mail.ru>
2022-01-26 17:06:04 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -122,6 +122,68 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    //Классы для карточек
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parent, ...clases) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.transfer = 27;
+            this.clases = clases;
+            this.changeToUAH();
+            this.parent = document.querySelector(parent);
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+        render() {
+            const element = document.createElement('div');
+            if (this.clases.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.clases.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+           <img src=${this.src} alt=${this.alt}>
+           <h3 class="menu__item-subtitle">${this.title}</h3>
+           <div class="menu__item-descr">${this.descr}</div>
+           <div class="menu__item-divider"></div>
+           <div class="menu__item-price">
+               <div class="menu__item-cost">Цена:</div>
+               <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+           </div>`;
+            this.parent.append(element);
+        }
+    }
+
+
+    const getResource = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+
+        return await res.json();
+    };
+
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({
+                img,
+                altimg,
+                title,
+                descr,
+                price
+            }) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+
     //Forms post
 
     const forms = document.querySelectorAll('form');
@@ -133,10 +195,22 @@ window.addEventListener("DOMContentLoaded", () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    }
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -151,19 +225,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
             const formData = new FormData(form); //если установлен FormData то нам не нужно setRequestHeader('Content-type', 'multipart/form-data') если отправляем джейсон то нужно request.setRequestHeader('Content-type', 'application/json');
 
-            const object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                    method: 'POST',
-                    headers: {
-                        'Content-type': 'application/json'
-                    },
-                    body: JSON.stringify(object)
-                })
-                .then(data => data.text())
+            postData(' http://localhost:3000/requests', json)
                 .then(data => {
                     console.log(data);
                     showThanksModal(message.success);
@@ -199,7 +263,50 @@ window.addEventListener("DOMContentLoaded", () => {
             hideModal(modal);
         }, 3000);
     }
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
+
+    //slider
+
+    const slides = document.querySelectorAll('.offer__slide');
+    const prev = document.querySelector('.offer__slider-prev');
+    const next = document.querySelector('.offer__slider-next');
+    const total = document.querySelector('#total');
+    const current = document.querySelector('#current');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10){
+        total.textContent = `0${slides.length}`;
+    }else{
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        
+        slides.forEach(item => item.style.display = 'none');
+        if (slideIndex < 10) {
+            current.textContent = `0${slideIndex}`
+        }else{
+            current.textContent = slideIndex;
+        }
+        slides[slideIndex - 1].style.display = 'block';
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    })
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    })
 });
